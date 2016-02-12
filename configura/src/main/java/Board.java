1c6545b77b2dd442fd1fea2af401df88568ea438
J4ken <jojje133@gmail.com>
2016-02-12 20:02:05 +0100

    Added some classes

---
+++ configura/src/main/java/Board.java
@@ -0,0 +1,15 @@
+package main.java;
+
+/**
+ * Created by Jacob on 2/12/2016.
+ */
+public class Board
+{
+    private int heigh;
+    private int width;
+
+
+    public Board(int height, int width) {
+	
+    }
+}
--- configura/src/main/java/run.java
+++ configura/src/main/java/run.java
@@ -4,9 +4,7 @@ package main.java;
  * Created by lukas on 2016-02-12.
  */
 
-/**
- * Hej
- */
 
-public class run {
+public class Run
+{
 }
